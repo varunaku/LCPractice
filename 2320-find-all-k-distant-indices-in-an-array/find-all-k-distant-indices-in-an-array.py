ddcd2d3c6fcf14f694da6407ddb15de98581e544
varunaku <105853053+varunaku@users.noreply.github.com>
2025-06-24 17:37:05 -0400

    Time: 174 ms (36.58%) | Memory: 17.2 MB (100.00%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.py
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.py
@@ -5,10 +5,24 @@ class Solution:
         # nums[j] == key
 
         # return all sorted ascending
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if abs(i - j) <= k and nums[j] == key:
+        # for i in range(len(nums)):
+        #     for j in range(len(nums)):
+        #         if abs(i - j) <= k and nums[j] == key:
+        #             res.add(i)
+
+
+        # return list(res)
+
+        # alternatively, break into 2 loops. First loop is to find all key indexes and store
+        # 2nd loop is to iterate through the 
+        n = len(nums)
+        for j in range(len(nums)):
+            if nums[j] == key:
+                start_bound = max(0, j-k) # to ensure no index below zero
+                end_bound = min(n-1, j+k)
+                for i in range(start_bound, end_bound+1):
                     res.add(i)
 
-
         return list(res)
+        # best solution. find difference from each key index (j) or +k / -k, and add both to set. 
+        # o(n) This way only one iteraiton needed.
