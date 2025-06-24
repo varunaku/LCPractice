84942c82e5fd75c21ae0cd506e4e30d3960d9ead
varunaku <105853053+varunaku@users.noreply.github.com>
2025-06-24 17:48:04 -0400

    Time: 3 ms (84.21%) | Memory: 17.9 MB (90.79%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.py
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
-        res = set()
+        res = []
         # |i - j| <= k
         # nums[j] == key
 
@@ -16,12 +16,19 @@ class Solution:
         # alternatively, break into 2 loops. First loop is to find all key indexes and store
         # 2nd loop is to iterate through the 
         n = len(nums)
+        marked_arr = [0] * (len(nums) +1)
         for j in range(len(nums)):
             if nums[j] == key:
                 start_bound = max(0, j-k) # to ensure no index below zero
                 end_bound = min(n-1, j+k)
-                for i in range(start_bound, end_bound+1):
-                    res.add(i)
+                marked_arr[start_bound] += 1
+                marked_arr[end_bound + 1] -= 1 # the boundaries get the subtraction to not count it.
+                    
+        count = 0
+        for i in range(len(marked_arr)):
+            count += marked_arr[i]
+            if count > 0:
+                res.append(i)
 
         return sorted(res)
         # best solution. find difference from each key index (j) or +k / -k, and add both to set. 
